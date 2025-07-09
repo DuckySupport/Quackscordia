0da3ef35cbdbe493c3c1a200cbf3c1101dae02b2
Troptop <87533146+NickIsADev@users.noreply.github.com>
2025-07-08 22:56:52 -0500

    Update VoiceManager.lua

--- libs/voice/VoiceManager.lua
+++ libs/voice/VoiceManager.lua
@@ -18,16 +18,26 @@ function VoiceManager:__init(client)
 end
 
 function VoiceManager:_prepareConnection(state, connection)
-	if not next(opus) then
-		return self._client:error('Cannot prepare voice connection: libopus not found')
-	end
-	if not next(sodium) then
-		return self._client:error('Cannot prepare voice connection: libsodium not found')
-	end
-	local socket = VoiceSocket(state, connection, self)
-	local url = 'wss://' .. state.endpoint:gsub(':%d*$', '')
-	local path = format('/?v=%i', GATEWAY_VERSION_VOICE)
-	return wrap(socket.connect)(socket, url, path)
+    if not next(opus) then
+        return self._client:error('Cannot prepare voice connection: libopus not found')
+    end
+    if not next(sodium) then
+        return self._client:error('Cannot prepare voice connection: libsodium not found')
+    end
+
+    -- --- ADD THIS LOGGING ---
+    self._client:info('VoiceManager: Preparing connection with state data:')
+    self._client:info('  guild_id: %s', state.guild_id)
+    self._client:info('  user_id: %s', state.user_id)
+    self._client:info('  session_id: %s', state.session_id)
+    self._client:info('  token: %s', state.token)
+    self._client:info('  endpoint: %s', state.endpoint)
+    -- --- END ADDITION ---
+
+    local socket = VoiceSocket(state, connection, self)
+    local url = 'wss://' .. state.endpoint:gsub(':%d*$', '')
+    local path = format('/?v=%i', GATEWAY_VERSION_VOICE)
+    return wrap(socket.connect)(socket, url, path)
 end
 
 return VoiceManager
